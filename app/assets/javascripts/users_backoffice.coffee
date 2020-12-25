8fb30291c5110578d8830012c4993f01b3d7b515
Marcel Pereira <mcleisson@gmail.com>
2020-12-25 11:57:48 -0300

    alteração de layout de user_backoffice

--- app/assets/javascripts/users_backoffice.coffee
+++ app/assets/javascripts/users_backoffice.coffee
@@ -1,3 +1,9 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+//= require jquery/dist/jquery.min
+//= require bootstrap/dist/js/bootstrap.bundle.min
+//= require fastclick/lib/fastclick
+//= require nprogress/nprogress
+//= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min
+//= require custom
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
-Rails.application.config.assets.precompile += %w( sb-admin-2.min.js sb-admin-2.min.css )
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.js users_backoffice.css )
+Rails.application.config.assets.precompile += %w( sb-admin-2.min.js sb-admin-2.min.css custom.js custom.css )
