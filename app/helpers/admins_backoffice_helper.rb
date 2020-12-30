95b1d596a03c13a9d19274b1ac54b7f3570bae09
Marcel Pereira <mcleisson@gmail.com>
2020-12-29 22:39:35 -0300

    Criado helper para tradução de atributos e model

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,13 @@
 module AdminsBackofficeHelper
+
+    def translate_attributes(object = nil, method = nil)
+        (object && method) ?  object.model.human_attribute_name(method) : "informe os parâmetros"
+        
+    end
+    
+    def translate_model_name(object = nil, method = nil)
+       (object && method) ? object.model_name.human(method) : "erro"
+        
+    end
+    
 end
